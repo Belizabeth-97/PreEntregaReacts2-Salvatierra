0b038067c8bb3cd999b98c86dd8b3271b4cf6fa3
Brenda Salvatierra <brendaelizabethsalvatierra@gmail.com>
2024-04-24 19:31:27 -0300

    MiAPI

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -2,7 +2,7 @@
 
 function CardProducts(props) {
   return (
-    <div className="card-products max-w-xs p-6 rounded-md shadow-md dark:bg-gray-100 dark:text-gray-900 hover:scale-105">
+    <div className="card-products bg-amber-100 max-w-xs p-6 rounded-md shadow-md hover:scale-105">
       <img src="/Hamburguesa-fondo-negro.jpg" alt="" className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"/>
       <h2 className="font-bold h-10 p-2 text-center">{props.nombre}</h2>
       <button type="button" className=" bg-amber-400 flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md  dark:text-gray-50">Comprar</button>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import ItemListContainer from "./ItemListContainer"
 function Main () {
     return (
         <main className="p-6 grow font-bold font bg-amber-200">
-            <h1 className="text-center bg-amber-500">Nuestros productos</h1>
+            <h1 className="text-center h-20 text-amber-950">Catálogo de productos</h1>
             <ItemListContainer></ItemListContainer>
         </main>
     )
