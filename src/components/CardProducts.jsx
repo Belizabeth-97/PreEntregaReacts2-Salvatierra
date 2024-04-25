2689266cf16f8bb6bef60966f4c1fe87f6bb65fe
Brenda Salvatierra <brendaelizabethsalvatierra@gmail.com>
2024-04-25 16:59:09 -0300

    contacto

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom"
 
+function CardProducts(producto) {
+  
+  const { nombre , imagen , id } = producto
 
-function CardProducts(props) {
   return (
     <div className="card-products bg-amber-100 max-w-xs p-6 rounded-md shadow-md hover:scale-105">
       <img src="/Hamburguesa-fondo-negro.jpg" alt="" className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"/>
-      <h2 className="font-bold h-10 p-2 text-center">{props.nombre}</h2>
-      <button type="button" className=" bg-amber-400 flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md  dark:text-gray-50">Comprar</button>
+      <h2 className="font-bold h-10 p-2 text-center">{nombre}</h2>
+      <Link to={"/" + id} className=" bg-amber-400 flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md  dark:text-gray-50">Comprar</Link>
+      
     </div>
   )
 }
